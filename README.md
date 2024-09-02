9f361beb20658d659dddf0477ee98e9225b5cb6d
Befrish <software@befrish.de>
2024-09-02 12:50:49 +0200

    copy vaadin canvas addon as base

---
+++ README.md
@@ -0,0 +1,55 @@
+Heavily inspired by [Canvas Java Addon](https://github.com/parttio/vaadin-flow-canvas)
+
+# Canvas Component with Sketch Feature for Vaadin 10+
+
+This is a Java integration of the HTML5 `<canvas>` for Vaadin.
+
+Currently, this add-on provides a subset of the [client-side JavaScript API](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API) to Java users.
+
+## Usage Example
+
+Add the dependency to your Vaadin 10+ project's `pom.xml`:
+```xml
+<dependency>
+    <groupId>org.parttio</groupId>
+    <artifactId>canvas-java</artifactId>
+    <version>2.0.0</version>
+</dependency>
+```
+
+The API is similar to its client-side counterpart:
+```java
+Canvas canvas = new Canvas(800, 500);
+CanvasRenderingContext2D ctx = canvas.getContext();
+
+// Draw a red line from point (10,10) to (100,100):
+ctx.setStrokeStyle("red");
+ctx.beginPath();
+ctx.moveTo(10, 10);
+ctx.lineTo(100, 100);
+ctx.closePath();
+ctx.stroke();
+```
+
+More examples can be found in the [demo sources](https://github.com/parttio/vaadin-flow-canvas/blob/master/src/test/java/org/vaadin/pekkam/DemoView.java).
+
+## Development Instructions
+
+Starting the test/demo server:
+```
+mvn jetty:run
+```
+
+This deploys the demo at `http://localhost:8080`
+
+## Cutting new release
+
+Before cutting a release, make sure the build passes properly locally and in GitHub Actions based verification build.
+
+To tag a release and increment versions, go the line-awesome subdirectory and issue:
+
+    mvn release:prepare release:clean
+
+Answer questions, defaults most often fine. Note that release:perform is not needed as there is a GitHub Action is set up build and to push release to Maven Central automatically.
+
+Directory will automatically pick up new releases within about half an hour, but if browser or Vaadin version support change, be sure to adjust the metadata in Vaadin Directory UI.
---
+++ src/main/java/org/vaadin/pekkam/Canvas.java
@@ -0,0 +1,157 @@
+package org.vaadin.pekkam;
+
+import com.vaadin.flow.component.*;
+import com.vaadin.flow.shared.Registration;
+import org.vaadin.pekkam.event.*;
+
+/**
+ * Canvas component that you can draw shapes and images on. It's a Java wrapper
+ * for the
+ * <a href="https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API">HTML5
+ * canvas</a>.
+ * <p>
+ * Use {@link #getContext()} to get API for rendering shapes and images on the
+ * canvas.
+ * <p>
+ */
+@Tag("canvas")
+@SuppressWarnings("serial")
+public class Canvas extends Component implements HasStyle, HasSize, KeyNotifier {
+    private CanvasRenderingContext2D context;
+
+    /**
+     * Creates a new canvas component with the given size.
+     * <p>
+     * Use the API provided by {@link #getContext()} to render graphics on the
+     * canvas.
+     * <p>
+     * The width and height parameters will be used for the canvas' coordinate
+     * system. They will determine the size of the component in pixels, unless
+     * you explicitly set the component's size with {@link #setWidth(String)} or
+     * {@link #setHeight(String)}.
+     * 
+     * @param width
+     *            the width of the canvas
+     * @param height
+     *            the height of the canvas
+     */
+    public Canvas(int width, int height) {
+        context = new CanvasRenderingContext2D(this);
+
+        getElement().setAttribute("width", String.valueOf(width));
+        getElement().setAttribute("height", String.valueOf(height));
+    }
+
+    /**
+     * Gets the context for rendering shapes and images in the canvas.
+     * <p>
+     * It is a Java wrapper for the <a href=
+     * "https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D">same
+     * client-side API</a>.
+     * 
+     * @return the 2D rendering context of this canvas
+     */
+    public CanvasRenderingContext2D getContext() {
+        return context;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <b>NOTE:</b> Canvas has an internal coordinate system that it uses for
+     * drawing, and it uses the width and height provided in the constructor.
+     * This coordinate system is independent of the component's size. Changing
+     * the component's size with this method may scale/stretch the rendered
+     * graphics.
+     */
+    @Override
+    public void setWidth(String width) {
+        HasSize.super.setWidth(width);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <b>NOTE:</b> Canvas has an internal coordinate system that it uses for
+     * drawing, and it uses the width and height provided in the constructor.
+     * This coordinate system is independent of the component's size. Changing
+     * the component's size with this method may scale/stretch the rendered
+     * graphics.
+     */
+    @Override
+    public void setHeight(String height) {
+        HasSize.super.setHeight(height);
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * <b>NOTE:</b> Canvas has an internal coordinate system that it uses for
+     * drawing, and it uses the width and height provided in the constructor.
+     * This coordinate system is independent of the component's size. Changing
+     * the component's size with this method may scale/stretch the rendered
+     * graphics.
+     */
+    @Override
+    public void setSizeFull() {
+        HasSize.super.setSizeFull();
+    }
+
+    /**
+     * Load an image resource an prepare it for use as a fill or stroke style. Since images are loaded
+     * asyncronously, you need to wait until you receive the associated ImageLoadEvent. Register an
+     * event listener using addImageLoadListener().
+     *
+     * @param src the path to the image resource
+     */
+    public void loadImage(String src)
+    {
+        getElement().executeJs("""
+        var img = new Image();
+        var self = this;
+        img.onload = function () {
+            if (!self.images) self.images = {};
+            self.images[$0] = img;
+            self.$server.imageLoaded($0);
+        };
+        img.src=$0;""",
+           src
+        );
+    }
+
+    @ClientCallable
+    public void imageLoaded(String src)
+    {
+        fireEvent(new ImageLoadEvent(this, true, src));
+    }
+
+    public Registration addMouseDownListener(ComponentEventListener<MouseDownEvent> listener)
+    {
+        return addListener(MouseDownEvent.class, listener);
+    }
+
+    public Registration addMouseUpListener(ComponentEventListener<MouseUpEvent> listener)
+    {
+        return addListener(MouseUpEvent.class, listener);
+    }
+
+    public Registration addMouseMoveListener(ComponentEventListener<MouseMoveEvent> listener)
+    {
+        return addListener(MouseMoveEvent.class, listener);
+    }
+
+    public Registration addMouseClickListener(ComponentEventListener<MouseClickEvent> listener)
+    {
+        return addListener(MouseClickEvent.class, listener);
+    }
+
+    public Registration addMouseDblClickListener(ComponentEventListener<MouseDblClickEvent> listener)
+    {
+        return addListener(MouseDblClickEvent.class, listener);
+    }
+
+    public Registration addImageLoadListener(ComponentEventListener<ImageLoadEvent> listener)
+    {
+        return addListener(ImageLoadEvent.class, listener);
+    }
+}
